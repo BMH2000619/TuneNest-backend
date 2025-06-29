372d6dac396e3d55497ae771a93f839002e9adde
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-29 07:28:01 +0300

    finished SongController for now

--- controllers/SongController.js
+++ controllers/SongController.js
@@ -6,21 +6,28 @@ const getAllSongs = async (req, res) => {
     const songs = await Song.find().populate('addedBy', 'username img')
     res.status(200).json(songs)
   } catch (err) {
-    res.status(500).json({ message: 'Failed to fetch songs', error:err.message })
+    res
+      .status(500)
+      .json({ message: 'Failed to fetch songs', error: err.message })
   }
 }
 
 // Get A Single Song By ID
 const getSongById = async (req, res) => {
   try {
-    const song = await Song.findById(req.params.id).populate('addedBy', 'username img')
+    const song = await Song.findById(req.params.id).populate(
+      'addedBy',
+      'username img'
+    )
     if (!song) {
       return res.status(404).json({ message: 'Song not found' })
     }
 
     res.status(200).json(song)
   } catch (err) {
-    res.status(500).json({ message: 'Failed to fetch song', error: err.message })
+    res
+      .status(500)
+      .json({ message: 'Failed to fetch song', error: err.message })
   }
 }
 
@@ -34,12 +41,68 @@ const createSong = async (req, res) => {
       artist,
       url,
       duration,
-      addedBy: req.user._id,
+      addedBy: req.user._id
     })
 
     res.status(291).json(newSong)
   } catch (err) {
-    res.status(400).json({ message: 'Failed to create song', error: err.message })
+    res
+      .status(400)
+      .json({ message: 'Failed to create song', error: err.message })
   }
 }
 
+// Update A Song
+const updateSong = async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.id)
+    if (!song) {
+      return res.status(404).json({ message: 'Song not found' })
+    }
+
+    if (!song.addedBy.equals(req.user._id)) {
+      return res.status(403).json({ message: 'Unauthorized' })
+    }
+
+    const { title, artist, url, duration } = req.body
+
+    song.title = title ?? song.title
+    song.artist = artist ?? song.artist
+    song.url = url ?? song.url
+    song.duration = duration ?? song.duration
+
+    await song.save()
+    res.status(200).json(song)
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Failed to update song', error: err.message })
+  }
+}
+
+// Delete A Song
+const deleteSong = async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.id)
+    if(!song) {
+      return res.status(404).json({ message: 'Song not found' })
+    }
+
+    if(!song.addedBy.equals(req.user._id)) {
+      return res.status(403).json({ message: 'Unauthorized' })
+    }
+
+    await song.remove()
+    res.status(200).json({ message: 'Song deleted successfully' })
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete song', error: err.message})
+  }
+}
+
+module.exports = {
+  getAllSongs,
+  getSongById,
+  createSong,
+  updateSong,
+  deleteSong,
+}
