51c5f0013084cef7568e871873b2bdecdefa0a1c
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-29 03:20:30 +0300

    updated playlist model

--- models/Playlist.js
+++ models/Playlist.js
@@ -5,11 +5,9 @@ const playlistSchema = new Schema({
   description: { type: String, default: '' },
   isPublic: { type: Boolean, required: true, default: false },
   coverImg: { type: String, default: '' },
-  createdAt: { type: Date, required: true, default: Date.now },
-  updatedAt: { type: Date, required: true, default: Date.now },
   createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   songs: [{ type: Schema.Types.ObjectId, ref: 'Song' }],
   likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-})
+}, { timestamps: true })
 
 module.exports = playlistSchema
